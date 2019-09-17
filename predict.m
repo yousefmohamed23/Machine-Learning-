dd5ab68dabe00e78743048b66a173107571750cd
yousefmohamed23 <36644002+yousefmohamed23@users.noreply.github.com>
2019-09-17 20:47:16 +0200

    Assignment

--- predict.m
+++ predict.m
@@ -1,26 +1,35 @@
-function p = predict(theta, X)
-%PREDICT Predict whether the label is 0 or 1 using learned logistic 
-%regression parameters theta
-%   p = PREDICT(theta, X) computes the predictions for X using a 
-%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
 
-m = size(X, 1); % Number of training examples
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly
-p = zeros(m, 1);
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+X=[ones(m,1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters. 
-%               You should set p to a vector of 0's and 1's
-H=  X * theta;
-g=sigmoid(H);
-pos=find(g>=0.5);
-p(pos)=1;
-
-
-
-
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+%Mapping from layer 1 to layer 2 
+z_2=Theta1*X';
+a_2=sigmoid(z_2);
+temp=ones(1,m);
+a_2_new=[temp ; a_2];
+z_3=Theta2*a_2_new;
+a_3=sigmoid(z_3);
+[value,index]=max(a_3,[],1); %return index of max num at every column 
+p=index';
 
 
 
--- sigmoid.m
+++ sigmoid.m
@@ -1,28 +1,6 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid function
-%   g = SIGMOID(z) computes the sigmoid of z.
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
-g = zeros(size(z));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar);
-%x=size(z);
-%m=x(1,1);
-%n=x(1,2);
-%for i=1:m
- % for j=1:n
-  %  g(i,j)=pinv(1+exp(-z(i,j)));
-  %endfor
-%endfor
-%Faster than above algorithem 
-ExpoTerm=1+exp(-z);
-g=ExpoTerm .^ -1;
-
-
-
-
-% =============================================================
-
+g = 1.0 ./ (1.0 + exp(-z));
 end
